322823ebda839339632e66b49953368831704418
S.M. Shovan <sm.shovan@gmail.com>
2021-06-23 20:37:17 +0600

    color

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const card = document.querySelector(".card");
 
 //Moving Animation Event
 card.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 1500;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 1500;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 1000;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 1000;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 //Animate In
 card.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
-card.style.transition = "all 3s ease";
+card.style.transition = "all 1s ease";
   //Popout
 });
 //Animate Out
 card.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 3s ease";
+  card.style.transition = "all 1s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   //Popback
 });
