69e09828404599b7caad4c28ba45133897566a52
S.M. Shovan <sm.shovan@gmail.com>
2021-06-24 21:18:29 +0600

    Header Background-3d pending

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 //Movement Animation to happen
-const card = document.querySelector(".card");
-
-//Items
+const card = document.querySelector("");
 
 
 //Moving Animation Event
 card.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 400;
-  let yAxis = (window.innerHeight / 4 - e.pageY) / 500;
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 550;
+  let yAxis = (window.innerHeight / 2 - 4*e.pageY) / 2050;
+  card.style.transform = `rotateY(${xAxis}deg) `;
 });
 //Animate In
 card.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
-card.style.transition = "all 2s ease";
-  //Popout
-});
+//Popout
+}); 
 //Animate Out
 card.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 2s ease";
+  card.style.transition = "all .5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
   //Popback
 });
